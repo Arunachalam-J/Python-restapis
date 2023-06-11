488c352c7a6a4425c14eaa9d5ff93dcf584be594
Arunachalam-J <jcarun28@gmail.com>
2023-06-11 12:04:12 +0530

    Pytest cases added to the project.

---
+++ app/tests/test_probes.py
@@ -0,0 +1,14 @@
+from app.db.set_setting import Mongo
+from app.resources.probes import readinessProbes, livenessProbes
+from flask import Response
+
+def test_readiness_probes():
+    readiness = readinessProbes()
+    response_message, response_code = readiness.get()
+    # Mongo clint not working
+    assert {"status":False},400 == readiness.get()
+    
+
+def test_liveness_probes():
+    liveness = livenessProbes()
+    assert {"status":True},200 == liveness.get() 
